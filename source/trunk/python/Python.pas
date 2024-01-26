97a5f275eb6b07d21d0d3b5462464e90414a5ab7
danielpharos <danielpharos@5419a3ea-6bc3-475c-b79b-167d7c3fbd5f>
2024-01-26 15:44:58 +0000

    Might not be a beta version, so change caption.
    git-svn-id: https://svn.code.sf.net/p/quark/code@8699 5419a3ea-6bc3-475c-b79b-167d7c3fbd5f

--- source/trunk/python/Python.pas
+++ source/trunk/python/Python.pas
@@ -1989,7 +1989,7 @@ end;
 procedure TestPythonObjectDump;
 begin
   if g_PythonObjects.Count>0 then
-    if Windows.MessageBox(0, 'Some Python objects were not correctly freed. This is a bug. Do you want to write a data report (PythonObjectDump.txt) ?', 'DEBUGGING - BETA VERSION', MB_YESNO) = IDYES then
+    if Windows.MessageBox(0, 'Some Python objects were not correctly freed. This is a bug. Do you want to write a data report (PythonObjectDump.txt) ?', 'QuArK - DEBUGGING', MB_YESNO) = IDYES then
       PythonObjectDump;
 end;
 {$ENDIF}
