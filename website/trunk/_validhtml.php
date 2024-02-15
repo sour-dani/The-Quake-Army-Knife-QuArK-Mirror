da838e0a5ba4f37a53b72fb4c872cd4888d2a822
danielpharos <danielpharos@5419a3ea-6bc3-475c-b79b-167d7c3fbd5f>
2024-02-15 21:48:32 +0000

    Small code improvement.
    git-svn-id: https://svn.code.sf.net/p/quark/code@8777 5419a3ea-6bc3-475c-b79b-167d7c3fbd5f

--- website/trunk/_validhtml.php
+++ website/trunk/_validhtml.php
@@ -8,9 +8,7 @@ require_once('_image_functions.php');
 $bodytext = '<div class="centered">';
 
 $bodytext .= '<div class="mouseover">';
-$bodytext .= '<a rel="nofollow noopener" target="_blank" href="http://validator.w3.org/check?uri=referer">';
-$bodytext .= DisplayImage('validhtml');
-$bodytext .= '</a>';
+$bodytext .= '<a rel="nofollow noopener" target="_blank" href="http://validator.w3.org/check?uri=referer">'.DisplayImage('validhtml').'</a>';
 $bodytext .= '<div class="popup">'.GetLanguageString('ValidHTML').'</div>';
 $bodytext .= '</div>';
 $bodytext .= '<br>';
@@ -18,9 +16,7 @@ $bodytext .= '<br>';
 $bodytext .= '<br>';
 
 $bodytext .= '<div class="mouseover">';
-$bodytext .= '<a rel="nofollow noopener" target="_blank" href="http://jigsaw.w3.org/css-validator/check/referer">';
-$bodytext .= DisplayImage('validcss');
-$bodytext .= '</a>';
+$bodytext .= '<a rel="nofollow noopener" target="_blank" href="http://jigsaw.w3.org/css-validator/check/referer">'.DisplayImage('validcss').'</a>';
 $bodytext .= '<div class="popup">'.GetLanguageString('ValidCSS').'</div>';
 $bodytext .= '</div>';
 $bodytext .= '<br>';
