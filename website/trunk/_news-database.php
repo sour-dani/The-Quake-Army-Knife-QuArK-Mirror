936832091b17bb9cfd9d788856807c2ee092856f
danielpharos <danielpharos@5419a3ea-6bc3-475c-b79b-167d7c3fbd5f>
2024-03-29 19:31:45 +0000

    Added latest news.
    git-svn-id: https://svn.code.sf.net/p/quark/code@8904 5419a3ea-6bc3-475c-b79b-167d7c3fbd5f

--- website/trunk/_news-database.php
+++ website/trunk/_news-database.php
@@ -381,4 +381,5 @@ $News[] = new cNews(6, mktime(0, 0, 0, 9, 28, 2023), NULL, 'news20230928.txt');
 $News[] = new cNews(6, mktime(0, 0, 0, 10, 14, 2023), NULL, 'news20231014.txt');
 $News[] = new cNews(6, mktime(0, 0, 0, 11, 18, 2023), NULL, 'news20231118.txt');
 $News[] = new cNews(6, mktime(0, 0, 0, 2, 18, 2024), NULL, 'news20240218.txt');
+$News[] = new cNews(6, mktime(0, 0, 0, 2, 28, 2024), NULL, 'news20240228.txt');
 ?>
