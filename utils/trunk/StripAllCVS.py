9f2200952bc44a0e0c3afcf7e66208d72771753f
danielpharos <danielpharos@5419a3ea-6bc3-475c-b79b-167d7c3fbd5f>
2025-03-15 15:06:45 +0000

    Small code improvement.
    git-svn-id: https://svn.code.sf.net/p/quark/code@9179 5419a3ea-6bc3-475c-b79b-167d7c3fbd5f

--- utils/trunk/StripAllCVS.py
+++ utils/trunk/StripAllCVS.py
@@ -6,75 +6,68 @@ import shutil
 import sys
 
 if len(sys.argv) != 2:
-    raise RuntimeError("Wrong number of arguments!")
-
-base_path = sys.argv[1]
+	raise RuntimeError("Usage: python %s <runtime path>" % (sys.argv[0], ))
+path = sys.argv[1]
 
 def DoFile(filename):
-    if not (filename.endswith(".py") or filename.endswith(".txt") or filename.endswith(".qrk")):
-        #Not a file we want/need to handle!
-        return
+	if (os.path.splitext(filename)[1] != ".py") and (os.path.splitext(filename)[1] != ".txt") and (os.path.splitext(filename)[1] != ".qrk"):
+		#Not a file we want/need to handle!
+		return
 
-    lines = []
-    file = open(filename, "r")
-    try:
-        for line in file.readlines():
-            lines += [line]
-    finally:
-        file.close()
+	lines = []
+	with open(filename, mode="r") as inFile
+		for line in inFile.readlines():
+			lines.append(line)
 
-    def getLinesToRemove(lines, identifier):
-        whatLines = []
-        lineIdentifier = -1
-        for i, line in enumerate(lines):
-            if line.find(identifier) != -1:
-                lineIdentifier = i
-                break
-        if lineIdentifier != -1:
-            index = lines[lineIdentifier].find(identifier)
-            #Everything in front of the identifier is the prepend-marker to use to figure out the size of the CVS information block.
-            marker = lines[lineIdentifier][:index]
-            if len(marker) != 0:
-                i = lineIdentifier - 1
-                while (i >= 0) and (lines[i].startswith(marker)):
-                    i -= 1
-                startOfBlock = i + 1
-                i = lineIdentifier + 1
-                while (i < len(lines)) and (lines[i].startswith(marker)):
-                    i += 1
-                endOfBlock = i - 1
-                for i in range(startOfBlock, endOfBlock + 1):
-                    whatLines += [i, ]
-            else:
-                whatLines += [lineIdentifier, ]
-        return whatLines
+	def getLinesToRemove(lines, identifier):
+		whatLines = []
+		lineIdentifier = -1
+		for i, line in enumerate(lines):
+			if line.find(identifier) != -1:
+				lineIdentifier = i
+				break
+		if lineIdentifier != -1:
+			index = lines[lineIdentifier].find(identifier)
+			#Everything in front of the identifier is the prepend-marker to use to figure out the size of the CVS information block.
+			marker = lines[lineIdentifier][:index]
+			if len(marker) != 0:
+				i = lineIdentifier - 1
+				while (i >= 0) and (lines[i].startswith(marker)):
+					i -= 1
+				startOfBlock = i + 1
+				i = lineIdentifier + 1
+				while (i < len(lines)) and (lines[i].startswith(marker)):
+					i += 1
+				endOfBlock = i - 1
+				for i in range(startOfBlock, endOfBlock + 1):
+					whatLines.append(i)
+			else:
+				whatLines.append(lineIdentifier)
+		return whatLines
 
-    removeLines = []
-    removeLines += getLinesToRemove(lines, "$Id:")
-    removeLines += getLinesToRemove(lines, "$Header")
-    removeLines += getLinesToRemove(lines, "$Log")
-    removeLines += getLinesToRemove(lines, "# ----------- REVISION HISTORY ------------") #Special case: This line too!
-    if len(removeLines) != 0:
-        file = open(filename, "w")
-        try:
-            for i, line in enumerate(lines):
-                if not i in removeLines:
-                    file.write(line)
-        finally:
-            file.close()
+	removeLines = []
+	removeLines += getLinesToRemove(lines, "$Id:")
+	removeLines += getLinesToRemove(lines, "$Header")
+	removeLines += getLinesToRemove(lines, "$Log")
+	removeLines += getLinesToRemove(lines, "# ----------- REVISION HISTORY ------------") #Special case: This line too!
+	if len(removeLines) != 0:
+		with open(filename, mode="w") as outFile:
+			for i, line in enumerate(lines):
+				if not i in removeLines:
+					outFile.write(line)
 
-def DoDir(dir):
-    for entry in os.scandir(dir):
-        if entry.is_dir():
-            if entry.name == "CVS":
-                shutil.rmtree(entry.path)
-            else:
-                DoDir(entry.path)
-        else:
-            if entry.name in (".cvsignore", ".project"):
-                os.remove(entry.path)
-            else:
-                DoFile(entry.path)
+def DoDir(path):
+	for entry in os.scandir(path):
+		if entry.is_dir():
+			if entry.name == "CVS":
+				shutil.rmtree(entry.path)
+			else:
+				DoDir(entry.path)
+		else:
+			if entry.name in set({".cvsignore", ".project"}):
+				os.remove(entry.path)
+			else:
+				DoFile(entry.path)
 
 raw_input("This will modify files and may delete entire directories in the given path! Are you sure? (BREAK the script if not!)")
-DoDir(base_path)
+DoDir(path)
