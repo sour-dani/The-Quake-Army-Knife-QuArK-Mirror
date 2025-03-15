ec5037966e125092675e3b2ae15d7bae66ceedae
danielpharos <danielpharos@5419a3ea-6bc3-475c-b79b-167d7c3fbd5f>
2025-03-15 14:44:12 +0000

    Added a script that lists the BSP header signature of BSP-files.
    git-svn-id: https://svn.code.sf.net/p/quark/code@9177 5419a3ea-6bc3-475c-b79b-167d7c3fbd5f

---
+++ utils/trunk/ExtractAllBSPSignatures.py
@@ -0,0 +1,25 @@
+#This script lists the BSP header signature of all the BSP-files in the given path.
+
+import os
+import struct
+import sys
+
+if len(sys.argv) != 2:
+	raise RuntimeError("Usage: python %s <path>" % (sys.argv[0], ))
+path = sys.argv[1]
+
+for entry in os.scandir(path):
+	if (os.path.splitext(entry.name)[1] != ".bsp") and (os.path.splitext(entry.name)[1] != ".d3dbsp"):
+		continue
+	with open(entry.path, mode="rb", buffering=0) as inFile:
+		inFile.seek(0, os.SEEK_END)
+		size = inFile.tell()
+		if size < 8:
+			print("WARNING: %s is too small!" % (entry.name, ))
+			continue
+		inFile.seek(0, os.SEEK_SET)
+		data = inFile.read(4)
+		signatureStr = struct.unpack("4s", data)[0]
+		signatureInt = struct.unpack("I", data)[0]
+		version = struct.unpack("I", inFile.read(4))[0]
+	print("%s: %s (0x%08x) - Version: 0x%08x" % (entry.name, signatureStr, signatureInt, version))
